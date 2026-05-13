5714eb5f16241879cb5f1935b08d537d2806921d
ishannkc <ishankc777@gmail.com>
2026-05-13 21:50:16 +0545

    initial commit: server & dependencies setup

---
+++ data/data.js
@@ -0,0 +1,92 @@
+export const albums = [
+    {
+        title: 'Off the Wall',
+        artist: 'Michael Jackson',
+        price: 19.99,
+        image: 'off-the-wall.png',
+        year: 1979,
+        era: 'Early Solo (1970s)',
+        genre: 'Disco / Funk / Soul',
+        stock: 50,
+        topSongs: [
+            "Don't Stop 'Til You Get Enough",
+            'Rock with You',
+            'Off the Wall'
+        ]
+    },
+    {
+        title: 'Thriller',
+        artist: 'Michael Jackson',
+        price: 24.99,
+        image: 'thriller.png',
+        year: 1982,
+        era: 'Epic Era (1980s)',
+        genre: 'Pop / R&B / Rock',
+        stock: 50,
+        topSongs: [
+            'Billie Jean',
+            'Beat It',
+            'Thriller'
+        ]
+    },
+    {
+        title: 'Bad',
+        artist: 'Michael Jackson',
+        price: 21.99,
+        image: 'bad.png',
+        year: 1987,
+        era: 'Epic Era (1980s)',
+        genre: 'Pop / R&B / Funk',
+        stock: 50,
+        topSongs: [
+            'Bad',
+            'Smooth Criminal',
+            'Man in the Mirror'
+        ]
+    },
+    {
+        title: 'Dangerous',
+        artist: 'Michael Jackson',
+        price: 21.99,
+        image: 'dangerous.png',
+        year: 1991,
+        era: 'New Jack Era (1990s)',
+        genre: 'New Jack Swing / R&B',
+        stock: 50,
+        topSongs: [
+            'Black or White',
+            'Remember the Time',
+            'Heal the World'
+        ]
+    },
+    {
+        title: 'HIStory: Past, Present and Future, Book I',
+        artist: 'Michael Jackson',
+        price: 26.99,
+        image: 'history.png',
+        year: 1995,
+        era: 'New Jack Era (1990s)',
+        genre: 'Pop / R&B / Gospel',
+        stock: 50,
+        topSongs: [
+            "They Don't Care About Us",
+            'Scream',
+            'Earth Song'
+        ]
+    },
+    {
+        title: 'Invincible',
+        artist: 'Michael Jackson',
+        price: 19.99,
+        image: 'invincible.png',
+        year: 2001,
+        era: 'Late Career (2000s)',
+        genre: 'R&B / Pop',
+        stock: 50,
+        topSongs: [
+            'You Rock My World',
+            'Butterflies',
+            'Break of Dawn'
+        ]
+    }
+]
