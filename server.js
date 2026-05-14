a8f79e745866b5ac93fd4f29f629ba9e82248e20
ishannkc <ishankc777@gmail.com>
2026-05-14 16:46:54 +0545

    feat: add user authentication endpoint to retrieve current user information

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import { productsRouter } from './routes/products.js'
 import { authRouter } from './routes/auth.js'
 import session from 'express-session'
+import { meRouter } from './routes/me.js'
 
 const app = express()
 const PORT = 8000
@@ -23,7 +24,7 @@ app.use(session({
 app.use(express.static('public'))
 
 app.use('/api/products', productsRouter)
-
+app.use('/api/auth/me', meRouter)
 app.use('/api/auth', authRouter)
 
 app.listen(PORT, ()=>{
